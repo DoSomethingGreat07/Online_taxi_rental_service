84366c7c654ce530de5a223cc2335948956b39b1
Nikhil Juluri <nikhiljuluri@Nikhils-MacBook-Air.local>
2025-05-01 16:48:48 -0500

    Adding readme file and requirements.txt files

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from database import get_connection  # Assuming you have a database connection setup
+from database import get_connection  
 
 app = Flask(__name__)
 CORS(app)
